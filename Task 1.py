5643270963c756440c72818a0132fe4535e9169a
Ravil <lllx2@yandex.ru>
2022-12-21 11:11:55 +0300

    2

--- Task 1.py
+++ Task 1.py
@@ -1,18 +1,19 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 num = float(input('Введите число '))
 sum = 0
-if num.is_integer():
+def get_sum(num, sum):
     while num > 0:
         n = num % 10
         sum += n
         num = num // 10
-    print(round(sum)) 
+    return sum
+
+if num.is_integer():
+    print(round(get_sum(num, sum)))
 else:
     s = str(num)
     z = (abs(s.find('.') - len(s)) - 1)
     num = num * (10**z)
-    while num > 0:
-        n = num % 10
-        sum += n
-        num = num // 10    
-    print(round(sum)) 
+    print(round(get_sum(num, sum)))
+
+
